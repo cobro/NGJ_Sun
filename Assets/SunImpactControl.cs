77a7134ea29b29627cbba93b2f17f7b8e9ae196f
Casper <caob1688@edu.kadk.dk>
2020-04-18 23:56:47 +0200

    sun effect

---
+++ Assets/SunImpactControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SunImpactControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField]
+
+    private VisualEffect visualEffect;
+
+    [SerializeField, Range(0, 2)]
+    private float Impact = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        visualEffect.SetFloat("Impact", Impact);
+    }
+}
