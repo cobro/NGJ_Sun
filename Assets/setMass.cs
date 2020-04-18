e0722ee0f33c4f4dbcae32aac0b5f5be477fd37c
Janek <amok.skull@gmail.com>
2020-04-18 14:25:52 +0200

    Mass

---
+++ Assets/setMass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setMass : MonoBehaviour
+{
+
+    Rigidbody rb;
+    float massCoefficient = 100;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = transform.localScale.x * massCoefficient;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
